4dc9a5b2171dc9c4910f4c205fb87054384290b9
GabContreras <contrerasgab01@gmail.com>
2024-06-03 11:57:51 -0600

    Varias validaciones

--- app/src/main/java/gabriel/contreras/validacionesyspinner/MainActivity.kt
+++ app/src/main/java/gabriel/contreras/validacionesyspinner/MainActivity.kt
@@ -1,6 +1,9 @@
 package gabriel.contreras.validacionesyspinner
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,54 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //1 Mandar a llamar a todos los elementos
+        val txtEdad = findViewById<EditText>(R.id.txtEdad)
+        val txtCorreo = findViewById<EditText>(R.id.txtCorreo)
+        val txtNombre = findViewById<EditText>(R.id.txtNombre)
+        val txtContrasena = findViewById<EditText>(R.id.txtContrasena)
+        val txtDUI = findViewById<EditText>(R.id.txtDui)
+        val btnAgregar = findViewById<Button>(R.id.btnAgregar)
+
+        //2 Agregar un evento al botón
+        btnAgregar.setOnClickListener {
+            //Validemos que los campos no estén vacios
+
+            if (txtEdad.text.isEmpty()
+                || txtCorreo.text.isEmpty()
+                || txtNombre.text.isEmpty()
+                || txtContrasena.text.isEmpty()
+                || txtDUI.text.isEmpty()
+            ) {
+                Toast.makeText(this, "Llene todos los campos", Toast.LENGTH_LONG).show()
+            }
+            //Validamos solo números
+            else if (txtEdad.text.matches("[0-9]+".toRegex())) {
+                Toast.makeText(this, "Campos agregados", Toast.LENGTH_LONG).show()
+            }
+            //Validar que correo lleve arroba
+            else {
+                Toast.makeText(this, "Verifique los campos", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        if (txtCorreo.text.matches("[a-zA-z0-9._-]+@[a-z]+\\\\.+[a-z]+]".toRegex())) {
+            Toast.makeText(this, "Correo real", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "Correo no real", Toast.LENGTH_LONG).show()
+        }
+
+        //Validar la contraseña para que tenga más de 6 digitos
+        if (txtContrasena.text.length <= 6){
+            Toast.makeText(this, "Debe tener más de 6 dígitos", Toast.LENGTH_LONG).show()
+        }
+
+        //validar el DUI
+        if (txtDUI.text.matches("[0-9]+-[0-9]".toRegex()) && txtDUI.text.length <=10)
+        {
+
+        }else {}
+
+        if(txtNombre.text.matches("[a-zA-z]=".toRegex())){}
     }
 }
